72862a444142a5c217cd2d5d906e3c8f79f6358d
DasithDeelaka <deelakajagoda@gmail.com>
2020-09-14 12:27:06 +0530

    Splash screen

--- client/App.js
+++ client/App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { Splash } from './components/SplashComponent';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	// Splash Screen with 1.5 seconds timeout when the app is being opened
+	const [timePassed, setTime] = useState(false);
+	setTimeout(() => {setTime(true)}, 1500);
+	if (!timePassed){
+		return (
+			<Splash />
+		);
+	} else {
+		return (
+			<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	}
 });
